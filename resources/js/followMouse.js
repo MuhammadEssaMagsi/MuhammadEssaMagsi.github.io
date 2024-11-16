1a20dc690bc49b890c9473fa343753cae5032773
Muhammad Essa Magsi <magsimuhammadessa@gmail.com>
2024-11-16 19:02:46 +0500

    MuhammadEssaMagsi.github.io first upload

---
+++ resources/js/followMouse.js
@@ -0,0 +1,14 @@
+const blob = document.getElementById('mouseBlob');
+
+
+document.body.onpointermove = event => {
+    const {clientX, clientY} = event;
+
+    // blob.style.left = `${clientX}px`;
+    // blob.style.top = `${clientY}px`;
+
+    blob.animate({ 
+        left: `${clientX}px`,
+        top: `${clientY}px`
+    }, {duration: 3000, fill: 'forwards'});
+}
